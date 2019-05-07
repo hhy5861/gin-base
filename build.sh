1700505da16fe3ac35d42d9c61b938912b4380bb
Mike <hhy5861@gmail.com>
2019-05-07 23:22:19 +0800

    init

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ APP_NAME=gin-base
 
 BIN_APP=bin/${APP_NAME}
 
-#if [[ -f "${BIN_APP}" ]];then
-#    echo "delete $BIN_APP ok"
-#    rm -f ${BIN_APP}
-#fi
-#
-#go build -v -o bin/${APP_NAME} .
-#
-#${BIN_APP} -c=config/local/config.yaml start
+if [[ -f "${BIN_APP}" ]];then
+    echo "delete $BIN_APP ok"
+    rm -f ${BIN_APP}
+fi
 
-gowatch -o ./bin/${APP_NAME} -p . -args='-c=config/local/config.yaml,start'
+go build -v -o bin/${APP_NAME} .
+
+${BIN_APP} -c=config/local/config.yaml start
+
+#gowatch -o ./bin/${APP_NAME} -p . -args='-c=config/local/config.yaml,start'
